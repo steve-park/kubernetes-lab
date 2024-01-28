c582fc4e7f220d2d0885dda30decbc2ca47af023
Steve-Park <deugsun.park@gmail.com>
2024-01-29 01:40:44 +0900

    Modifying Vagrantfile & common.sh
    1. Update OS from ubuntu 20.04 to ubuntu 23.04
    2. Update docker Latest version (2023.01.29) & SystemdCgroup Setting
    3. Include cri-dockerd

--- Vagrantfile
+++ Vagrantfile
@@ -3,32 +3,32 @@
 
 Vagrant.configure("2") do |config|
 
-  config.vm.define "storage" do |storage|
-    storage.vm.hostname = "storage"
-    storage.vm.box = "bento/ubuntu-20.04"
-    # master.vm.box_check_update = false
+  # config.vm.define "storage" do |storage|
+  #   storage.vm.hostname = "storage"
+  #   storage.vm.box = "bento/ubuntu-23.04"
+  #   # master.vm.box_check_update = false
 
-    storage.vm.network "private_network", ip: "192.168.56.210"
+  #   storage.vm.network "private_network", ip: "192.168.56.210"
 
-    # config.vm.synced_folder "../data", "/vagrant_data"
+  #   # config.vm.synced_folder "../data", "/vagrant_data"
 
-    storage.vm.provider "virtualbox" do |vb|
-      vb.gui = false
-      vb.name = "storage"
-      vb.cpus = 2
-      vb.memory = "1024"
-    end
+  #   storage.vm.provider "virtualbox" do |vb|
+  #     vb.gui = false
+  #     vb.name = "storage"
+  #     vb.cpus = 2
+  #     vb.memory = "1024"
+  #   end
 
-    storage.vm.provision "shell", path: "./scripts/storage.sh"
+  #   storage.vm.provision "shell", path: "./scripts/storage.sh"
 
-    storage.vm.provision "shell", privileged: false, inline: <<-SHELL
-      echo "nfs storage server is ready!!!"
-    SHELL
-  end
+  #   storage.vm.provision "shell", privileged: false, inline: <<-SHELL
+  #     echo "nfs storage server is ready!!!"
+  #   SHELL
+  # end
 
   config.vm.define "master" do |master|
     master.vm.hostname = "master"
-    master.vm.box = "bento/ubuntu-20.04"
+    master.vm.box = "bento/ubuntu-23.04"
     # master.vm.box_check_update = false
 
     master.vm.network "private_network", ip: "192.168.56.200"
@@ -43,46 +43,46 @@ Vagrant.configure("2") do |config|
     end
 
     master.vm.provision "shell", path: "./scripts/common.sh"
-    master.vm.provision "shell", path: "./scripts/master.sh"
+    # master.vm.provision "shell", path: "./scripts/master.sh"
 
-    master.vm.provision "shell", privileged: false, inline: <<-SHELL
-      mkdir -p $HOME/.kube
-      sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-      sudo chown $(id -u):$(id -g) $HOME/.kube/config
-      echo "source <(kubectl completion bash)" >> $HOME/.bashrc
-      echo "source <(kubeadm completion bash)" >> $HOME/.bashrc
+    # master.vm.provision "shell", privileged: false, inline: <<-SHELL
+    #   mkdir -p $HOME/.kube
+    #   sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    #   sudo chown $(id -u):$(id -g) $HOME/.kube/config
+    #   echo "source <(kubectl completion bash)" >> $HOME/.bashrc
+    #   echo "source <(kubeadm completion bash)" >> $HOME/.bashrc
 
-      echo "kubernetes master node is ready!!!"
-    SHELL
+    #   echo "kubernetes master node is ready!!!"
+    # SHELL
   end
 
-  (1..2).each do |i|
-    config.vm.define "worker-#{i}" do |worker|
-      worker.vm.hostname = "worker-#{i}"
-      worker.vm.box = "bento/ubuntu-20.04"
+  # (1..2).each do |i|
+  #   config.vm.define "worker-#{i}" do |worker|
+  #     worker.vm.hostname = "worker-#{i}"
+  #     worker.vm.box = "bento/ubuntu-20.04"
 
-      worker.vm.network "private_network", ip: "192.168.56.20#{i}"
+  #     worker.vm.network "private_network", ip: "192.168.56.20#{i}"
 
-      worker.vm.provider "virtualbox" do |vb|
-        vb.gui = false
-        vb.name = "worker-#{i}"
-        vb.cpus = 2
-        vb.memory = "2048"  
-      end
+  #     worker.vm.provider "virtualbox" do |vb|
+  #       vb.gui = false
+  #       vb.name = "worker-#{i}"
+  #       vb.cpus = 2
+  #       vb.memory = "2048"  
+  #     end
 
-      worker.vm.provision "shell", path: "./scripts/common.sh"
-      worker.vm.provision "shell", path: "./scripts/worker.sh"
+  #     worker.vm.provision "shell", path: "./scripts/common.sh"
+  #     worker.vm.provision "shell", path: "./scripts/worker.sh"
 
-      worker.vm.provision "shell", privileged: false, inline: <<-SHELL
-        mkdir -p $HOME/.kube
-        sudo cp -i /vagrant/config/admin.conf $HOME/.kube/config
-        sudo chown $(id -u):$(id -g) $HOME/.kube/config
-        echo "source <(kubectl completion bash)" >> $HOME/.bashrc
-        echo "source <(kubeadm completion bash)" >> $HOME/.bashrc
+  #     worker.vm.provision "shell", privileged: false, inline: <<-SHELL
+  #       mkdir -p $HOME/.kube
+  #       sudo cp -i /vagrant/config/admin.conf $HOME/.kube/config
+  #       sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  #       echo "source <(kubectl completion bash)" >> $HOME/.bashrc
+  #       echo "source <(kubeadm completion bash)" >> $HOME/.bashrc
         
-        echo "kubernetes worker node is ready!!!"
-      SHELL
-    end
-  end
+  #       echo "kubernetes worker node is ready!!!"
+  #     SHELL
+  #   end
+  # end
 
 end
--- scripts/common.sh
+++ scripts/common.sh
@@ -3,11 +3,11 @@
 # configure name resolution
 echo "" >> /etc/hosts
 echo "# Kubernetes Lab" >> /etc/hosts
-echo "192.168.56.210    storage" >> /etc/hosts
 echo "192.168.56.200    master" >> /etc/hosts
 echo "192.168.56.201    worker-1" >> /etc/hosts
 echo "192.168.56.202    worker-2" >> /etc/hosts
 echo "192.168.56.203    worker-3" >> /etc/hosts
+echo "192.168.56.210    storage" >> /etc/hosts
 
 # configure kubernetes requirements
 swapoff -a
@@ -17,50 +17,84 @@ cp /etc/fstab /etc/fstab.bak
 egrep -v swap /etc/fstab.bak > /etc/fstab
 
 cat <<EOF | tee /etc/modules-load.d/k8s.conf
+overlay
 br_netfilter
 EOF
+
 cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward                 = 1
 EOF
+
 sysctl --system
 
 systemctl stop ufw
 systemctl disable ufw
 
 # install & configure docker
-apt-get update
-apt-get remove -y docker docker-engine docker.io containerd runc
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
+do 
+    NEEDRESTART_MODE=a apt-get remove -y $pkg 2> /dev/null
+done
 
-apt-get install -y ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+NEEDRESTART_MODE=a apt-get update
 
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
-usermod -aG docker vagrant
+NEEDRESTART_MODE=a apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+NEEDRESTART_MODE=a apt-get update
+NEEDRESTART_MODE=a apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+usermod -aG docker vagrant > /dev/null
 systemctl enable docker.service > /dev/null
 systemctl enable containerd.service > /dev/null
 
-# install & configure kubectl
-apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+mv /etc/containerd/config.toml /etc/containerd/config.toml.bak 2> /dev/null
+sh -c "containerd config default" > /etc/containerd/config.toml
+sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
 
-apt-get update
-apt-get install -y kubelet kubeadm kubectl bash-completion nfs-common
-apt-mark hold kubelet kubeadm kubectl
+# install cri-dockerd
+wget -qO- https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.9/cri-dockerd-0.3.9.amd64.tgz | tar xvz
+# mv ./cri-dockerd/cri-dockerd /usr/local/bin/
+mv ./cri-dockerd/cri-dockerd /usr/bin/
+rm -rf ./cri-dockerd/ > /dev/null 2> /dev/null
 
-systemctl enable kubelet
-systemctl start kubelet
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service -O /etc/systemd/system/cri-docker.service
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket -O /etc/systemd/system/cri-docker.socket
+# mv cri-docker.socket cri-docker.service /etc/systemd/system/ 
+# sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
+systemctl status cri-docker.socket
 
-# configure auto completion
-echo "source <(kubectl completion bash)" >> $HOME/.bashrc
-echo "source <(kubeadm completion bash)" >> $HOME/.bashrc
-echo 'source /usr/share/bash-completion/bash_completion' >> $HOME/.bashrc
+# # install & configure kubectl
+# apt-get install -y apt-transport-https ca-certificates curl
+# curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+# echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 
-# configure local persistent volume
-mkdir -p /data/local-pv01
-mkdir -p /data/local-pv02
-mkdir -p /data/local-pv03
+# apt-get update
+# apt-get install -y kubelet kubeadm kubectl bash-completion nfs-common
+# apt-mark hold kubelet kubeadm kubectl
+
+# systemctl enable kubelet
+# systemctl start kubelet
+
+# # configure auto completion
+# echo "source <(kubectl completion bash)" >> $HOME/.bashrc
+# echo "source <(kubeadm completion bash)" >> $HOME/.bashrc
+# echo 'source /usr/share/bash-completion/bash_completion' >> $HOME/.bashrc
+
+# # configure local persistent volume
+# mkdir -p /data/local-pv01
+# mkdir -p /data/local-pv02
+# mkdir -p /data/local-pv03
